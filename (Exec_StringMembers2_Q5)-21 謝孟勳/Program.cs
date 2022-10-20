c9dd05c5d9327a8ead3b7b9933004068e7848146
ispan <ispan@DESKTOP-PAKSETB>
2022-10-20 12:35:17 +0800

    加入專案檔案。

---
+++ (Exec_StringMembers2_Q5)-21 謝孟勳/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _Exec_StringMembers2_Q5__21_謝孟勳
+{
+    //比對使用者輸入值是否為 "Allen" 不拘大小寫
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Allen Kou";
+            if (Regex.IsMatch(name, "[A-Z]"))
+            {
+                Console.WriteLine("是不拘大小寫");
+            }
+        }
+    }
+}
